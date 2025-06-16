10d1e2dbe4375ad8cc53f15d939a9674c1ecfa5c
Mesut Yildiz <80417121+mstylz@users.noreply.github.com>
2025-06-16 19:08:27 -0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,27 @@
-const initialCards = [
-  {
-    name: "Val Thorens",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg ",
-  },
-  {
-    name: "Restaurant terrace",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
-  },
-  {
-    name: "An outdoor cafe ",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
-  },
-  {
-    name: "A very long bridge, over the forest and through the trees",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
-  },
-  {
-    name: "Tunnel with morning light",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
-  },
-  {
-    name: "Mountain house",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
-  },
+const initialCards = [{
+        name: "Val Thorens",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg ",
+    },
+    {
+        name: "Restaurant terrace",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+    },
+    {
+        name: "An outdoor cafe ",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+    },
+    {
+        name: "A very long bridge, over the forest and through the trees",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+    },
+    {
+        name: "Tunnel with morning light",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+    },
+    {
+        name: "Mountain house",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+    },
 ];
 
 const editProfileBtn = document.querySelector(".profile__edit-btn");
@@ -31,10 +30,10 @@ const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 const editProfileForm = editProfileModal.querySelector(".modal__form");
 
 const editProfileNameInput = editProfileModal.querySelector(
-  "#profile-name-input"
+    "#profile-name-input"
 );
 const editProfileDescriptionInput = editProfileModal.querySelector(
-  "#profile-description-input"
+    "#profile-description-input"
 );
 
 const newPostBtn = document.querySelector(".profile__new-post-btn");
@@ -44,28 +43,41 @@ const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEl = document.querySelector(".profile__description");
 
-editProfileBtn.addEventListener("click", function () {
-  editProfileNameInput.value = profileNameEl.textContent;
-  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
+function openModal(modal) {
+    modal.classList.add("modal_is-opened");
+}
 
-  editProfileModal.classList.add("modal_is-opened");
+function closeModal(modal) {
+    modal.classList.remove("modal_is-opened");
+}
+
+editProfileBtn.addEventListener("click", function() {
+    editProfileNameInput.value = profileNameEl.textContent;
+    editProfileDescriptionInput.value = profileDescriptionEl.textContent;
+
+    //editProfileModal.classList.add("modal_is-opened");
+    openModal(editProfileModal);
 });
-editProfileCloseBtn.addEventListener("click", function () {
-  editProfileModal.classList.remove("modal_is-opened");
+editProfileCloseBtn.addEventListener("click", function() {
+    //editProfileModal.classList.remove("modal_is-opened");
+    closeModal(editProfileModal);
 });
 
-newPostBtn.addEventListener("click", function () {
-  newPostModal.classList.add("modal_is-opened");
+newPostBtn.addEventListener("click", function() {
+    //newPostModal.classList.add("modal_is-opened");
+    openModal(newPostModal);
 });
-newPostCloseBtn.addEventListener("click", function () {
-  newPostModal.classList.remove("modal_is-opened");
+newPostCloseBtn.addEventListener("click", function() {
+    //newPostModal.classList.remove("modal_is-opened");
+    closeModal(newPostModal);
 });
 
 function handleEditProfileSubmit(evt) {
-  evt.preventDefault();
-  profileNameEl.textContent = editProfileNameInput.value;
-  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
-  editProfileModal.classList.remove("modal_is-opened");
+    evt.preventDefault();
+    profileNameEl.textContent = editProfileNameInput.value;
+    profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+    //editProfileModal.classList.remove("modal_is-opened");
+    closeModal(editProfileModal);
 }
 editProfileForm.addEventListener("submit", handleEditProfileSubmit);
 
@@ -74,15 +86,16 @@ const newPostImageInput = document.querySelector("#card-image-input");
 const newPostCaptionInput = document.querySelector("#new-post-caption-input");
 
 function handleNewPostSubmit(evt) {
-  evt.preventDefault();
-  console.log("Image URL:", newPostImageInput.value);
-  console.log("Caption:", newPostCaptionInput.value);
-  newPostModal.classList.remove("modal_is-opened");
+    evt.preventDefault();
+    console.log("Image URL:", newPostImageInput.value);
+    console.log("Caption:", newPostCaptionInput.value);
+    //newPostModal.classList.remove("modal_is-opened");
+    closeModal(newPostModal);
 }
 
 newPostForm.addEventListener("submit", handleNewPostSubmit);
 
-initialCards.forEach(function (item) {
-  console.log(item.name);
-  console.log(item.link);
+initialCards.forEach(function(item) {
+    console.log(item.name);
+    console.log(item.link);
 });
