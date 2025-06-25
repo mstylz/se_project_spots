c020e3c9c0bfc21599874cd313b9318f342893d3
Mesut Yildiz <80417121+mstylz@users.noreply.github.com>
2025-06-25 12:31:35 -0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,3 @@
-import { initialCards } from './cards.js';
-
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
